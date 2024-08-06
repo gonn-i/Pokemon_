c7d5b3ec2674fca245652a11e3defe566d76f239
goeun kim <rlarhdms6828@naver.com>
2024-08-07 08:21:50 +0900

    Chore: 패키지 설치 및 일부 스타일 조정

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -5,24 +5,33 @@ import './type';
 export const GlobalStyles = createGlobalStyle`
 ${reset};
 
-  * {
-    // 드래그 하지 못하게
-    user-select: none; 
-    -webkit-user-drag: none;
-    -moz-user-drag: none;
-    -o-user-drag: none;
-    user-drag: none;
-  }
+  // * {
+  //   // 드래그 하지 못하게
+  //   user-select: none; 
+  //   -webkit-user-drag: none;
+  //   -moz-user-drag: none;
+  //   -o-user-drag: none;
+  //   user-drag: none;
+  // }
 
   body {
     margin: 0;
     padding: 0;
   }
 
+  a{
+    text-decoration: none; 
+    color: inherit; 
+  
+  }
+
   :root {
     //폰트 크기
-    --font-size-large: 27px; 
+    --font-size-extralarge: 45px; 
+    --font-size-large: 30px; 
+    --font-size-stat: 20px; 
     --font-size-normal: 13px; 
+    --font-size-small: 10px; 
 
     //폰트 굵기
     --font-weight-types: 300;
@@ -34,11 +43,16 @@ ${reset};
     //폰트 색
     --text-color-main: #FFFFFF;
     --text-color-grey: #949494;
+    --text-color-black: #000000;
 
     //토큰 버튼 색
     --toggle-background-light:#fcd659;
     --toggle-background-dark: #333;
 
+
+    //박스 그림자
+    --box-shadow-normal: rgba(0, 0, 0, 0.15) 4px 10px 10px 4px;
+    --text-shadow-normal: 5px 5px #858585b0;
   }
 
   // 폰트 
@@ -48,4 +62,11 @@ ${reset};
       font-size: x-large;
       font-style: normal;
   }
+
+  @font-face {
+    font-family: 'DungGeunMo';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/DungGeunMo.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
 `;
